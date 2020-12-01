c7b1f1aa3d6fed61b9a337a748bf4b3daadea463
gmolin <gmolin@student.hive.fi>
2020-12-01 23:22:07 +0200

    also improved read_champ file: it includes new lines now + removed legacy code

--- srcs/asm/main.c
+++ srcs/asm/main.c
@@ -33,18 +33,17 @@ char		*read_champ(char *champ)
 	line = NULL;
 	if (((fd = open(champ, O_RDONLY)) == -1))
 		print_error(INVALID_FILE);
-	s = ft_get_next_line(fd, &line) - 1;
+	s = 1024;
 	new_buffer(&buffer, s);
-	if (!(insert_buffer(&buffer, line, s - 1)))
-		print_error(BUFFER);
 	while ((s = ft_get_next_line(fd, &line)) > 0)
 	{
-		if (!(insert_buffer(&buffer, line, s - 1)))
+		line[s - 1] = '\n';
+		if (!(insert_buffer(&buffer, line, s)))
 			print_error(BUFFER);
 		ft_strdel(&line);
 	}
 	printf("%s\n", buffer.data); //delete
-	if (ft_get_next_line(fd, &line) < 0)
+	if (s < 0)
 		print_error(GET_NEXT_LINE);
 	return (buffer.data);
 }
