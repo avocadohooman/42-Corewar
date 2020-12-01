254ea906127b2d371b5ce3b6de8885e93f2ad0d0
Oskari Rantanen <oskari@Oskari-MacBook-Pro.local>
2020-12-01 14:41:01 +0200

    all types to begin with

---
+++ includes/asm.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   asm.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: orantane <orantane@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/01 13:50:53 by orantane          #+#    #+#             */
+/*   Updated: 2020/12/01 14:33:18 by orantane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ASM_H
+# define ASM_H
+# include "libft.h"
+# include "op.h"
+# include "error.h"
+# include <stdio.h> // REMOVE BEFORE TURNING IN!!
+# include <sys/uio.h>
+# include <sys/types.h>
+
+void		print_error(char *msg);
+
+#endif
---
+++ includes/error.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seronen <seronen@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/01 14:26:33 by orantane          #+#    #+#             */
+/*   Updated: 2020/12/01 14:2826 by seronen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ERROR_H
+# define ERROR_H
+# include "libft.h"
+
+char	*g_errors[] = {"Usage: ./asm [champion_file.s]", "Missing argument!",
+						"three"};
+
+typedef enum	e_error_type
+{
+	USAGE,
+	MISSING_ARG,
+	INVALID_TOKEN
+}				t_error_type;
+
+void	print_error(t_error_type type);
+
+#endif
