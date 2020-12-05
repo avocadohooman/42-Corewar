865885cf53663f1ee3b8c803750449579ba5f655
Oskari Rantanen <orantane@student.hive.fi>
2020-12-05 16:53:45 +0200

    Updated the file, to the newest version

--- srcs/asm/encoder.c
+++ srcs/asm/encoder.c
@@ -6,44 +6,75 @@
 /*   By: orantane <orantane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/04 13:51:56 by orantane          #+#    #+#             */
-/*   Updated: 2020/12/04 15:55:19 by orantane         ###   ########.fr       */
+/*   Updated: 2020/12/05 16:53:05 by orantane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "file.h"
 #include "asm.h"
 #include "error.h"
 
-t_file		*encode_int(t_file *buf, int champ_size, int i)
+static unsigned char	*encode_int(unsigned char *out, int i, int champ_size)
 {
-	unsigned int	nbr;
-	int				len;
+ 	int		*nbr;
+	int		len;
+	int		magic;
+	int		j;
 
+    j = 0;
+    magic = COREWAR_EXEC_MAGIC;
 	len = 4;
-	if (champ_size)
-		nbr = champ_size;
+	if (champ_size > 0)
+		nbr = &champ_size;
 	else if (champ_size == -1)
-		nbr = COREWAR_EXEC_MAGIC;
+		nbr = &magic;
 	else if (champ_size == 0)
 		nbr = 0;
 	while (--len >= 0)
-		buf->data[i + len] = *(((unsigned char *)nbr) + len);
-	return (buf);
+	{
+		out[i + j] = *(((unsigned char*)nbr) + len);
+        j++;
+	}
+	return (out);
 }
 
-t_file		*encode_string(t_file *buf)
+static unsigned char	*encode_string(unsigned char *out, int i, 
+										unsigned char *text, int type)
 {
-	return (buf);
+	int		index;
+
+	index = 0;
+	while (text && text[index])
+	{
+		if (type == 1 && index == PROG_NAME_LENGTH)
+			print_error("Champion's name is too long!");
+		if (type == 2 && index == COMMENT_LENGTH)
+			print_error("Comment is too long");
+		out[i + index] = text[index];
+		index++;
+	}
+	while (type == 1 && index < PROG_NAME_LENGTH + 4)
+		out[i + index++] = 0;
+	while (type == 2 && index < COMMENT_LENGTH + 4)
+		out[i + index++] = 0;
+	return (out);
 }
 
-void		encode_output(t_file *buf, int champ_size)
+unsigned char			*encode_output(t_header *strings, int champ_size)
 {
-	int		i;
+	unsigned char	*out;
+	int				i;
+	size_t			size;
 
-	encode_int(buf, 0, 0);
+	size = 16 + PROG_NAME_LENGTH + COMMENT_LENGTH;
+	i = 0;
+	out = ft_memalloc(size);
+	i = 0;
+	out = encode_int(out, i, -1);
+	i += sizeof(int);
+	out = encode_string(out, i, strings->prog_name, 1);
+	i += PROG_NAME_LENGTH + 4;
+	out = encode_int(out, i, champ_size);
 	i += 4;
-	encode_string(buf);
-	encode_int(buf, champ_size, i);
-	encode_string(buf);
-	return (buf);
+	out = encode_string(out, i, strings->comment, 2);
+	return (out);
 }
