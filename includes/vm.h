6699c5f255b21716355b9341bffc0812d50917a3
Samuli <samuli841@gmail.com>
2021-01-13 20:34:06 +0200

    vm arguments read with correct position flags

--- includes/vm.h
+++ includes/vm.h
@@ -3,14 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   vm.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orantane <orantane@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: seronen <seronen@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 13:51:26 by orantane          #+#    #+#             */
-/*   Updated: 2020/12/01 15:36:02 by orantane         ###   ########.fr       */
+/*   Updated: 2021/01/13 20:30:53 by seronen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VM_H
 # define VM_H
 
+# include "op.h"
+
+typedef struct  s_player
+{
+	int				id;
+	char            *file_name;
+}               t_player;
+
+typedef struct  s_vm
+{
+	int			cur_id;
+	t_player	*players[MAX_PLAYERS + 1];
+}               t_vm;
+
 #endif
+
+
+
+
+
+
+
+
+
+
+
+/*
+int val = 2|4;  // = 6
+
+r1 = 1
+
+result = val & 1 = 3
+
+1 != 3 
+_________
+
+%1 = 2
+
+result = val & 2 = 2
+
+2 = 2 YEEESSS
+
+*/
--- srcs/vm/main.c
+++ srcs/vm/main.c
@@ -11,39 +11,128 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "buffer.h"
+#include "vm.h"
 #include <stdio.h>
 
-int main(void)
+/*
+	#include "buffer.h"
+	t_buffer b;
+
+	printf("hello vm\n");
+	if (!new_buffer(&b, 10))
+		return (1);
+
+	for (int i = 0; i < 20; i++)
+	{
+		if (!insert_buffer(&b, "asd ", 4))
+			return (1);
+	}
+
+	printf("%s\n", b.data);
+	printf("%zu\n", b.size);
+	printf("%zu\n", b.used);
+
+	// t_test  a;
+	// t_test  c;
+
+	// a.value = 123;
+	// c.value = 321;
+
+	// llist_add((t_llist *)a, (t_llist *)c);
+
+*/
+
+void		error(char *msg)
 {
-    t_buffer b;
+	printf("%s", msg);
+	exit(0);
+}
 
-    printf("hello vm\n");
-    if (!new_buffer(&b, 10))
-        return (1);
+void		new_player(t_vm *vm, int id, char *name)
+{
+	t_player *new;
 
-    for (int i = 0; i < 20; i++)
-    {
-        if (!insert_buffer(&b, "asd ", 4))
-            return (1);
-    }
+	new = malloc(sizeof(t_player));
+	new->file_name = name;
+	new->id = id;
+	vm->players[id - 1] = new;
+}
 
-    printf("%s\n", b.data);
-    printf("%zu\n", b.size);
-    printf("%zu\n", b.used);
+int		add_first(t_vm *vm, int player, char **args, int ac)
+{
+	int i;
 
-    // t_test  a;
-    // t_test  c;
+	i = 1;
+	while (i < ac)
+	{
+		if (args[i] && !ft_strcmp(args[i], "-n"))
+			i += 2;
+		else if (args[i])
+		{
+			new_player(vm ,player, ft_strdup(args[i]));
+			args[i] = NULL;
+			return (0);
+		}
+		i++;
+	}
+	return (0);
+}
 
-    // a.value = 123;
-    // c.value = 321;
+int     arguments(t_vm *vm, int player, char **args, int ac)
+{
+	int i;
+	int pos;
 
-    // llist_add((t_llist *)a, (t_llist *)c);
+	i = 0;
+	while (i++ < ac)
+	{
+		if (args[i] && !ft_strcmp(args[i], "-n"))
+		{
+			pos = ft_atoi(args[i + 1]);
+			if (pos > MAX_PLAYERS)
+				error("Poitional argument over limit!\n");
+			if (pos == player)
+			{
+				new_player(vm ,player, ft_strdup(args[i + 2]));
+				bzero(&args[i], sizeof(char*) * 3);
+				return (0);
+			}
+			else
+				i += 2;
+		}
+	}
+	return (add_first(vm, player, args, ac));
+}
 
+int		get_positions(t_vm *vm, char **args, int ac)
+{
+	int player;
 
+	player = 0;
+	while (player < MAX_PLAYERS)
+	{
+		arguments(vm, player + 1, args, ac);
+		player++;
+	}
+	return (0);
+}
 
+int     main(int ac, char **av)
+{
+	t_vm *vm;
 
-
-
-    return (0);
+	vm = malloc(sizeof(t_vm));
+	vm->cur_id = 1;
+	bzero(vm->players, sizeof(t_player) * MAX_PLAYERS + 1);
+	get_positions(vm, av, ac);
+	printf("\nPLAYER POSITIONS\n");
+	if (vm->players[0])
+		printf("\n1: Name: %s, id: %d\n", vm->players[0]->file_name, vm->players[0]->id);
+	if (vm->players[1])
+		printf("2: Name: %s, id: %d\n", vm->players[1]->file_name, vm->players[1]->id);
+	if (vm->players[2])
+		printf("3: Name: %s, id: %d\n", vm->players[2]->file_name, vm->players[2]->id);
+	if (vm->players[3])
+		printf("4: Name: %s, id: %d\n", vm->players[3]->file_name, vm->players[3]->id);
+	return (0);
 }
