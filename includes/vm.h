d0a5323643fe3fba135097684f15a0131e98e691
Samuli <samuli841@gmail.com>
2021-02-04 23:16:58 +0200

    arg_type_code decrypter ready, t_statement logic ready

--- includes/vm.h
+++ includes/vm.h
@@ -3,17 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vm.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-<<<<<<< HEAD
 /*   By: seronen <seronen@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 13:51:26 by orantane          #+#    #+#             */
-/*   Updated: 2021/02/03 13:51:12 by seronen          ###   ########.fr       */
-=======
-/*   By: gmolin <gmolin@student.hive.fi>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/01 13:51:26 by orantane          #+#    #+#             */
-/*   Updated: 2021/02/03 19:25:19 by gmolin           ###   ########.fr       */
->>>>>>> master
+/*   Updated: 2021/02/04 23:04:55 by seronen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,50 +34,32 @@ typedef struct  s_player
 	unsigned char	*exec_code;
 }               t_player;
 
-<<<<<<< HEAD
-typedef struct	s_statement
+typedef struct  s_stmt
 {
-int					bytes_to_next_statement;
-unsigned char 		statement;
-unsigned char		arg_type;
-unsigned char		*arguments;
-}				t_statement;
+   int              bytes_to_next_statement;
+   int				cost;
+   unsigned char	statement;
+   unsigned char	arg_type;
 
-typedef struct	s_carry
-{
-	int				pos;
-	int				cycles_to_execution;
-	int				carry_flag;
-	int				regs[REG_NUMBER];
-	int				last_live;
-	t_statement		*statement;
-struct s_carry   *next;
-}				t_carry;
-=======
+   int				arg_types[3];
+   int				args[3];
+}               t_stmt;
+
 typedef struct  s_carriage 
 {
-	int                 pos;
+	unsigned char		*pos;
 	int                 cycles_to_execute;
 	int                 carry_flag;
 	int                 regs[REG_NUMBER];
 	int                 last_live;
-	// t_statement     *statement;
+	t_stmt     			*stmt;
 	struct s_carriage  *next;
 }               t_carriage;
-
-typedef struct  s_statement
-{
-   int              bytes_to_next_statement;
-   //<something>    statment
-   //<something>    arguments;
-}               t_statement;
->>>>>>> master
 
 typedef struct  s_vm
 {
 	int			dump;
 	int			player_nb;
-	t_carry		*carries;
 	t_player	*players[MAX_PLAYERS + 1];
     t_carriage  *carriages;
 }               t_vm;
@@ -95,7 +70,8 @@ typedef struct  s_vm
 int					get_players(t_vm *vm, char **args, int ac);
 int					read_files(t_vm *vm);
 unsigned char       *init_arena(t_vm *vm);
-void				initiate_carriages(t_vm *vm);
+void				initiate_carriages(t_vm *vm, unsigned char *arena);
+int     			form_statement(t_carriage *carry);
 
 #endif
 
--- srcs/vm/decrypt.c
+++ srcs/vm/decrypt.c
@@ -6,7 +6,7 @@
 /*   By: seronen <seronen@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 14:48:04 by seronen           #+#    #+#             */
-/*   Updated: 2021/02/04 15:52:45 by seronen          ###   ########.fr       */
+/*   Updated: 2021/02/04 23:15:27 by seronen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,98 +14,125 @@
 #include <stdio.h> // DELETE
 #include "opcodes.h"
 
+int		convert_4_bytes(unsigned char *data)
+{
+	// Converts 4 individual bytes to an int, counting for endianness
+	// Big endian to little endian
+
+	return ((data[0] << 24) | (data[1] << 16) | (data[2] << 8) | data[3]);
+}
+
 void	decrypt_arg_type(t_stmt *stmt, int counter)
 {
-	int z = 128;
+	// Cycles through bits in arg_type_code and finds arg types for each arg
+	// Includes types in arg_types array carriage->arg_types[3]
+	// Reviews two bits at a time
+
+	// 01 is T_REG
+	// 10 is T_DIR
+	// 11 is T_IND
+
+	int z = 128;	// ????
 	int oct = stmt->arg_type;
 
 	while (z > 0)
 	{
-		if (oct & z)
+		if (oct & z)	// if bit is 1
 		{
-			z >>= 1;
-			if (oct & z)
-			{
-				printf("Arg was T_IND\n");
+			z >>= 1;	// next bit
+			if (oct & z)	// if bit is 1
 				stmt->arg_types[counter] = T_IND;
-			}
 			else
-			{
-				printf("Arg was T_DIR\n");
 				stmt->arg_types[counter] = T_DIR;
-			}
 		}
 		else
 		{
-			z >>= 1;
-			if (oct & z)
-			{
-				printf("Arg was T_REG\n");
+			z >>= 1;	// next bit
+			if (oct & z)	// if bit is 1
 				stmt->arg_types[counter] = T_REG;
-			}
 			else
-			{
-				printf("Arg was NONE\n");
 				stmt->arg_types[counter] = 0;
-			}
 		}
-		z >>= 1;
-		counter++;
+		z >>= 1;	// next bit
+		counter++;	// counter to keep up with carriage->arg_types[3] array
 	}
 }
 
-void	init_statement(t_carry *carry)
+void	init_stmt(t_carriage *carry)
 {
 	t_stmt *stmt;
 
 	if (!(stmt = (t_stmt*)malloc(sizeof(t_stmt))))
 	{
-		printf("MALLOC ERROR!!!!!\n");
+		printf("MALLOC ERROR!!!!! vm/init_stmt\n");
 		exit(-42);
 	}
 	stmt->statement = carry->pos[0];
 	stmt->arg_type = carry->pos[1];
 	ft_bzero(stmt->arg_types, sizeof(int) * 3);
-	carry->statement = stmt;
+	carry->stmt = stmt;
 }
 
-int		read_arg(unsigned char *mem, int type, int *pos, unsigned char stmt)
+int		read_arg(unsigned char *mem, int type, int pos, unsigned char stmt)
 {
-	int to_read;
-
-	to_read = 0;
 	if (type == T_REG)
-		return ((int)mem[*pos]);
-	else if (type == T_DIR)
-		to_read = opcode_table[stmt].dir_size;
-	else if (type == T_IND)
-		to_read = 2;
+		return ((int)mem[pos]);						// convert 1 byte to an int
+	else if (type == T_DIR && opcode_table[stmt - 1].dir_size == 4)
+		return (convert_4_bytes(&mem[pos]));			// convert 4 bytes to an int
+	else
+		return ((mem[pos] << 8) + mem[pos + 1]);		// convert 2 bytes to an int
 }
 
-void	get_args(t_carry *carry, int pos)
+int		get_args(t_carriage *carry, int pos)
 {
 	int i;
 
 	i = 0;
 	while (i < 3)
 	{
-		carry->statement->args[i] = read_arg(carry->pos,
-			carry->statement->arg_types[i], &pos, carry->statement->statement);
-		printf("read_arg returned: %d\n", carry->statement->args[i]);
+		if (!carry->stmt->arg_types[i])		// Dont read if no arg, no arg type so no arg...
+			break ;
+		carry->stmt->args[i] = read_arg(carry->pos,
+			carry->stmt->arg_types[i], pos, carry->stmt->statement); 	// read arg and save it to args array
+		if (carry->stmt->arg_types[i] == T_REG)		// increment position
+			pos += 1;
+		else if (carry->stmt->arg_types[i] == T_DIR &&
+			opcode_table[carry->stmt->statement - 1].dir_size == 4) 		// increment position
+			pos += 4;
+		else
+			pos += 2;			// increment position
 		i++;
 	}
+	return (pos);
 }
 
-int     form_statement(t_vm *vm, t_carry *carry)
+int     form_statement(t_carriage *carry)
 {
 	int pos;
 
-    init_statement(carry);
+    init_stmt(carry);		// Init statement, mallocs and sets the statement
 	pos = 1;
-	if (opcode_table[carry->statement->statement].argument_type)
+	if (opcode_table[carry->stmt->statement - 1].argument_type) // If arg_type is true
 	{
-		decrypt_arg_type(carry->statement, 0); // zero param is just a counter :) saves lines!
+		decrypt_arg_type(carry->stmt, 0); // zero param is just a counter :) saves lines!
 		pos++;
 	}
-	get_args(carry, pos);
+	else
+		carry->stmt->arg_types[0] = T_DIR;	// Resolve single arg issue when no arg type present
+											// Only case when no arg_type_code is when first and only arg is T_DIR
+	carry->pos += get_args(carry, pos);		// Update carriage "PC" to the next statement
+	printf("Next statement code: %02x\n", carry->pos[0]);
+	printf("Statement code: %02x, numerical: %d\n", carry->stmt->statement, carry->stmt->statement);
+	printf("Arguments and types:\n");
+	int i = 0;
+	while (i < 3)
+	{
+		if (carry->stmt->arg_types[i])
+			printf("ARG %d == type %d –– value: %d\n", i + 1, carry->stmt->arg_types[i], carry->stmt->args[i]);
+		else
+			printf("No arg %d!\n", i + 1);
+		i++;
+	}
+	printf("\n");
+	return (0);
 }
