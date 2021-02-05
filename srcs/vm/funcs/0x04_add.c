83839f7f66698707a5d74a5dc79d2d9a7c8eccbe
Gerhard Molin <gerhard@minimumbadass.com>
2021-02-05 14:57:10 +0100

    renaming files

--- srcs/vm/funcs/0x04_add.c
+++ srcs/vm/funcs/0x04_add.c
@@ -6,9 +6,11 @@
 /*   By: Gerhard <Gerhard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 15:48:11 by Gerhard           #+#    #+#             */
-/*   Updated: 2021/02/05 15:49:14 by Gerhard          ###   ########.fr       */
+/*   Updated: 2021/02/05 15:52:43 by Gerhard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "vm.h"
 
 void    add(t_carriage *carriage, unsigned char *arena)
 {
