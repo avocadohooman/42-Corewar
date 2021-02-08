2a8bb714a58cc66b9088a71ec705dd766d692f0c
Samuli <samuli841@gmail.com>
2021-02-09 00:51:41 +0200

    uninitialized variable usage detected -> undefined values prevented

--- srcs/vm/funcs/0x05_sub.c
+++ srcs/vm/funcs/0x05_sub.c
@@ -6,7 +6,7 @@
 /*   By: seronen <seronen@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 15:48:14 by Gerhard           #+#    #+#             */
-/*   Updated: 2021/02/08 15:00:08 by seronen          ###   ########.fr       */
+/*   Updated: 2021/02/09 00:50:44 by seronen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void    op_sub(t_carriage *carriage)
 {
 	int     sum;
 
-	sum += carriage->regs[carriage->stmt->args[0]];
+	sum = carriage->regs[carriage->stmt->args[0]];
 	sum -= carriage->regs[carriage->stmt->args[1]];
 	carriage->regs[carriage->stmt->args[2]] = sum;
 	if (sum == 0)
