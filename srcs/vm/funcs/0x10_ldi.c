c828862b18aeb60ff4493be63820e92ef0bf40e1
gmolin <gmolin@student.hive.fi>
2021-02-13 23:50:28 +0200

    forgot to comment something / delete something

--- srcs/vm/funcs/0x10_ldi.c
+++ srcs/vm/funcs/0x10_ldi.c
@@ -6,7 +6,7 @@
 /*   By: gmolin <gmolin@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/06 15:09:30 by seronen           #+#    #+#             */
-/*   Updated: 2021/02/13 19:48:28 by gmolin           ###   ########.fr       */
+/*   Updated: 2021/02/13 23:49:18 by gmolin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,6 @@ void    op_ldi(t_carriage *carriage, unsigned char *arena)
 	reg_slot = carriage->stmt->args[2] - 1;
 	arg_value_1 = get_arg_value(carriage, 0, arena);
 	arg_value_2 = get_arg_value(carriage, 1, arena);
-    if (carriage->stmt->arg_types[0] == T_IND)
-    {
-        arg_value_2 = real_modulo(carriage->abs_pos, arg_value_2, IDX_MOD);
-        args_idx_value = arg_value_1 + arg_value_2;
-    }
-    else 
-        args_idx_value = real_modulo(carriage->abs_pos, (arg_value_1 + arg_value_2), IDX_MOD);
+    args_idx_value = real_modulo(carriage->abs_pos, (arg_value_1 + arg_value_2), IDX_MOD);
 	carriage->regs[reg_slot] = read_bytes(arena, args_idx_value, 4);
 }
--- srcs/vm/funcs/0x11_sti.c
+++ srcs/vm/funcs/0x11_sti.c
@@ -6,7 +6,7 @@
 /*   By: gmolin <gmolin@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/06 15:09:27 by seronen           #+#    #+#             */
-/*   Updated: 2021/02/13 19:48:15 by gmolin           ###   ########.fr       */
+/*   Updated: 2021/02/13 23:49:26 by gmolin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,6 @@ void    op_sti(t_carriage *carriage, unsigned char *arena)
 	arg_value_1 = carriage->regs[reg_slot];
 	arg_value_2 = get_arg_value(carriage, 1, arena);
 	arg_value_3 = get_arg_value(carriage, 2, arena);
-    if (carriage->stmt->arg_types[1] == T_IND)
-    {
-        arg_value_3 = real_modulo(carriage->abs_pos, arg_value_3, IDX_MOD);
-        args_idx_value = arg_value_3 + arg_value_2;
-    }
-    else 
-	    args_idx_value = real_modulo(carriage->abs_pos, (arg_value_2 + arg_value_3), IDX_MOD);
+    args_idx_value = real_modulo(carriage->abs_pos, (arg_value_2 + arg_value_3), IDX_MOD);
 	write_bytes(arena, args_idx_value, arg_value_1, 4);
 }
