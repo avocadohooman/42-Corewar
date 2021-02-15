20eb633f5297be7848a6b4d492ad969c809a258e
Samuli <samuli841@gmail.com>
2021-02-15 13:52:28 +0200

    compiled some champs, cleaning repo

--- resources/champs/zork_with_ld.s
+++ resources/champs/zork_with_ld.s
@@ -2,8 +2,9 @@
 .comment "This city needs me"
 
 loop:   
-        sti r1, %7, %1
+        sti r1, %14, %1
+        ld %4294902016, r4
 live:   
         live %0
         ld %0, r2
-        zjmp %-19
+        zjmp %-26
