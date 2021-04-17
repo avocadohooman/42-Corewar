a535130a8fa5af03ba245899dd3aa4549a924716
niklas <pimenoff.niklas@gmail.com>
2021-04-17 14:04:07 +0300

    removed t_header redefinition

--- includes/op.h
+++ includes/op.h
@@ -52,12 +52,12 @@
 
 typedef char			t_arg_type;
 
-typedef	struct			s_header
-{
-	unsigned int		magic;
-	char				prog_name[PROG_NAME_LENGTH + 1];
-	unsigned int		prog_size;
-	char				comment[COMMENT_LENGTH + 1];
-}						t_header;
+// typedef	struct			s_header
+// {
+// 	unsigned int		magic;
+// 	char				prog_name[PROG_NAME_LENGTH + 1];
+// 	unsigned int		prog_size;
+// 	char				comment[COMMENT_LENGTH + 1];
+// }						t_header;
 
 #endif
