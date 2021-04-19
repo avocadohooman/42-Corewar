b253a1866f55053ba9bd89dcae93315e05514eb5
Gerhard Molin <gerhard@minimumbadass.com>
2021-04-19 10:09:03 +0200

    rm old resources folder

--- resources/champs/error_zork.s
+++
@@ -1,9 +0,0 @@
-.name "B_atman"
-.comment "I will f***ing destroy your VM with my invalid sti 1st arg registry value!"
-
-loop:   
-        sti r0, %7, %1
-live:   
-        live %0
-        ld %0, r2
-        zjmp %-19
--- resources/champs/examples/zork.s
+++
@@ -1,8 +0,0 @@
-.name "zork"
-.comment "I'M ALIIIIVE"
-
-l2:		sti r1, %:live, %1
-		ld %0, r2
-
-live:	live %1
-		zjmp %:l2
