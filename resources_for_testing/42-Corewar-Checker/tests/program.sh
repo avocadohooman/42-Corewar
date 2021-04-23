a09406780ecf367a1e40244236777c15597a7d0b
Gerhard Molin <gerhard@minimumbadass.com>
2021-04-23 11:49:51 +0200

    cleaning up final submission

--- resources_for_testing/42-Corewar-Checker/tests/program.sh
+++
@@ -1,4 +0,0 @@
-./test.sh -c 18293 champions/md5.cor
-./test.sh -c 25902 champions/helltrain.cor champions/fluttershy.cor
-./test.sh -c 25902 champions/fluttershy.cor champions/toto.cor champions/leapfrog.cor
-./test.sh -c 10000 champions/le_pet_de_nonne.cor champions/Car.cor champions/helltrain.cor
